009f30f332ad87d50ec945f937fcb4e25361b573
kysongardner <kysongardner1@gmail.com>
2021-03-23 11:02:54 -0600

    added signup

---
+++ js/signUp.js
@@ -0,0 +1,26 @@
+function convertToJson(res){
+    if (res.ok){
+        return res.json()
+    } else{
+        let jsonReponse = res.json()
+        throw { name: "servicesError", message: jsonResponse}
+    }
+}
+
+document.querySelector(".signup-form").addEventListener("submit", signUp)
+
+async function signUp(e){
+    e.preventDefault()
+    e.target
+    // convert e.target to form data
+    const formData = new FormData(e.target)
+    const signUp = {
+        method: 'POST',
+        body: formData,
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+    }
+    const response = await fetch("https://trivia-api-cse-341.herokuapp.com/api/register", signUp).then(convertToJson)
+    console.log(response)
+    return response
+    
+}
