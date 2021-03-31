af95b6e358253478eadae15defd32818df67fe1a
kysongardner <kysongardner1@gmail.com>
2021-03-30 19:19:42 -0600

    fixed up addquestion

--- js/addQuestion.js
+++ js/addQuestion.js
@@ -14,11 +14,76 @@ document.querySelector(".add-question-form").addEventListener("submit", addQuest
 
 async function addQuestion(e) {
     e.preventDefault()
-    e.target
 
-    
+    // Get the category
+    let categoryOption = document.querySelector("#topics")
+    let categoryOptionValue = categoryOption.value
 
-    const formData = {}
+    // Get the difficulty
+    let difficultyOption = document.querySelector("#difficulty")
+    let difficultyOptionValue = difficultyOption.value
+
+    // Get the correctAnswer
+    let correctAnswerOption = document.querySelector("#correct-answer")
+    let correctAnswerOptionValue = correctAnswerOption.value
+
+
+
+
+    const question = document.querySelector("#question-input").value
+    const answerOne = document.querySelector("#answer-one").value
+    const answerTwo = document.querySelector("#answer-two").value
+    const answerThree = document.querySelector("#answer-three").value
+    const answerFour = document.querySelector("#answer-four").value
+    const category = categoryOptionValue
+    const difficulty = difficultyOptionValue
+    const correctAnswer = correctAnswerOptionValue
+
+    let firstAnswer = false
+    let secondAnswer = false
+    let thirdAnswer = false
+    let fourthAnswer = false
+
+    // Setting the correct answer attribute to the right answer
+    if (correctAnswer == 1) {
+        firstAnswer = true
+    }
+    if (correctAnswer == 2) {
+        secondAnswer = true
+    }
+    if (correctAnswer == 3) {
+        thirdAnswer = true
+    }
+    if (correctAnswer == 4) {
+        fourthAnswer = true
+    }
+
+    const formData = {
+        question: question,
+        answers: [{
+                text: answerOne,
+                isTrue: firstAnswer
+            },
+            {
+                text: answerTwo,
+                isTrue: secondAnswer
+            },
+            {
+                text: answerThree,
+                isTrue: thirdAnswer
+            },
+            {
+                text: answerFour,
+                isTrue: fourthAnswer
+            },
+
+        ],
+        category: category,
+        difficulty: `'${difficulty}'`
+    }
+
+    console.log(formData)
+
     const addQuestion = {
         method: 'POST',
         body: JSON.stringify(formData),
@@ -28,11 +93,4 @@ async function addQuestion(e) {
     }
     const response = await fetch("https://trivia-api-cse-341.herokuapp.com/api/submit-question", addQuestion).then(convertToJson)
     console.log(response)
-    if (response.isAuth) {
-        return response
-    } else {
-        alert("Error Adding Question! Please Try Again!")
-    }
-
-
 }
